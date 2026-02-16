8a414d6cd2a74bf35bf5da696109833b8209ff2f
JunSeo <37623843+jun562@users.noreply.github.com>
2026-02-16 17:52:31 +0900

    [Bronze I] Title: 소인수분해, Time: 188 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,22 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
 
-        if (N == 1) {
-            return;
-        }
-        
-        while (N != 1) {
-            for (int i = 2; i <= N; i++) {
-                if (N % i == 0) {
-                    System.out.println(i);
-                    N /= i;
-                    break;
-                }
+        // 1. i * i <= N 까지만 반복 (루트 N 최적화)
+        for (int i = 2; i * i <= N; i++) {
+            // i로 나누어 떨어지는 동안 계속 나눔 (소인수분해)
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
             }
+        }
+
+        // 2. 반복문 종료 후 N이 1보다 크다면, 
+        // 그 남은 수는 소수이므로 출력해야 함
+        if (N > 1) {
+            System.out.println(N);
         }
     }
 }
