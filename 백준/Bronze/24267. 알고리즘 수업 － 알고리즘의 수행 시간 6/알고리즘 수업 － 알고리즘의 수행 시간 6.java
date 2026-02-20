b98c46904c9a9f2a27c9f87fda9f43cb8ee435ed
JunSeo <37623843+jun562@users.noreply.github.com>
2026-02-20 16:06:42 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 152 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextInt();
+
+        System.out.println((n * (n - 1) * (n - 2)) / 6);
+        System.out.println(3);
+    }
+}
