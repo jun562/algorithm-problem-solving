92de3354349f5c8b2b5283a3aefe357b1af7dea8
JunSeo <37623843+jun562@users.noreply.github.com>
2026-05-17 00:42:30 +0900

    [level 2] Title: H-Index, Time: 3.85 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,34 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int solution(int[] citations) {
-        // 배열 정렬
-        Arrays.sort(citations);
-
-        // h-index 계산
-        int hIndex = 0;
+        int answer = 0; //정답
         int n = citations.length;
-        for (int i = 0; i < n; i++) {
-            int h = n - i; // 남은 논문의 개수
-            if (citations[i] >= h) {
-                hIndex = h;
+        int[] result = new int[citations.length];
+        
+        for(int i = 0; i< citations.length; i++){
+            result[i] = citations[i];
+        } //초기화 꼭 해주기
+        Arrays.sort(result);
+        
+        int count = 0;
+        int h = 0;
+        while(true){
+            for(int i = 0; i < result.length; i++){
+                if(result[i] >= h){
+                    count ++;
+                }
+            }
+            if(count >= h){
+                count = 0;
+                h++;
+            } else {
+                answer = h - 1;
                 break;
             }
         }
-
-        // h-index 반환
-        return hIndex;
+        
+        return answer;
     }
 }
+//발표 n 편중, h번 이상 인용된 논문이 h편 이상, 나머지 논문(n-h)이 h번 이하 인용되었다면 h의 최댓값 
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 1.84 ms
+메모리: 62.3 MB, 시간: 3.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 27일 11:10:36
+2026년 05월 17일 00:42:24
 
 ### 문제 설명
 
